f7a17e96a4f908bb996530de67af4479a4151b71
Marios Kerasiotis <marioskerasiotis@gmail.com>
2020-05-07 13:41:06 +0300

    make stayhome

--- ex2/stayhome/stayhome.py
+++
@@ -1,8 +0,0 @@
-def get_testcase(filename):
-    with open(filename) as f:
-        content = f.readlines()
-        content = [[c for c in x if c != '\n'] for x in content]
-        return content
-
-
-print(get_testcase("./tests-input/test1.txt"))
